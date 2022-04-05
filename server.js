f2fa18574c98b0ce494e481de4e7c33ed2752ced
megan <mtpott23@gmail.com>
2022-04-05 13:09:12 -0400

    front-end added

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const { animals } = require('./data/animals');
 
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
-
 //parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,6 +110,24 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//this route should always come last or else it will take precedence over
+//named routes, and you won't see what you expect to on those routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
